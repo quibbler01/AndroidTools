6dbd1f092ed20839522c1675383e82c9b2319d70
Quibbler <614449644@qq.com>
2021-04-23 18:51:09 +0800

    2021-04-24 修改调试模块名和包名为com.quibbler.toolstest，和工具库包名com.quibbler.androidtools区别

--- ToolsTestModule/src/main/java/com/quibbler/toolstest/TestActivity.kt
+++ ToolsTestModule/src/main/java/com/quibbler/toolstest/TestActivity.kt
@@ -1,11 +1,13 @@
-package com.quibbler.androidtools
+package com.quibbler.toolstest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class TestActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_test)
     }
+
 }
