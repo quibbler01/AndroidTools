e471724065d25f7cbfc719bfba150c8e5d87749a
quibbler01 <a2639168845@163.com>
2021-12-03 22:01:26 +0800

    2021-12-05 使用 getter 方法而不是属性访问语法：Use of getter method instead of property access syntax

--- AndroidTools/src/main/java/com/quibbler/androidtools/view/Density.kt
+++ AndroidTools/src/main/java/com/quibbler/androidtools/view/Density.kt
@@ -12,7 +12,7 @@ import android.view.WindowManager
  * @return Int value in px
  */
 fun dip2px(context: Context, dpValue: Float): Int {
-    val scale: Float = context.getResources().getDisplayMetrics().density
+    val scale: Float = context.resources.displayMetrics.density
     return (dpValue * scale + 0.5f).toInt()
 }
 
@@ -24,7 +24,7 @@ fun dip2px(context: Context, dpValue: Float): Int {
  * @return Int value in dp
  */
 fun px2dip(context: Context, pxValue: Float): Int {
-    val scale: Float = context.getResources().getDisplayMetrics().density
+    val scale: Float = context.resources.displayMetrics.density
     return (pxValue / scale + 0.5f).toInt()
 }
 
