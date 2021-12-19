c7062fbfa680dd423eb8f5b7186f783a13ace13a
quibbler01 <a2639168845@163.com>
2021-12-19 18:47:09 +0800

    2021-12-19 启动ViewBinding，增加测试界面

--- ToolsTestModule/src/main/java/com/quibbler/toolstest/TestActivity.kt
+++ ToolsTestModule/src/main/java/com/quibbler/toolstest/TestActivity.kt
@@ -2,21 +2,35 @@ package com.quibbler.toolstest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.quibbler.androidtools.packages.settings.openAppDetails
+import com.quibbler.androidtools.packages.settings.openInstalledAppDetails
+import com.quibbler.toolstest.databinding.ActivityTestBinding
 import com.quibbler.toolstest.test.InitTest
 import com.quibbler.toolstest.test.PackageUtilTest
 import com.quibbler.toolstest.test.ResUtilTest
 
 class TestActivity : AppCompatActivity() {
 
+    lateinit var testBinding: ActivityTestBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_test)
+
+        testBinding = ActivityTestBinding.inflate(layoutInflater)
+        setContentView(testBinding.root)
 
         InitTest().test()
 
         ResUtilTest.test(this)
 
         PackageUtilTest().test()
+
+        testBinding.settings.setOnClickListener {
+            openAppDetails(this)
+        }
+        testBinding.settingsOther.setOnClickListener {
+            openInstalledAppDetails(this, "com.tencent.mm")
+        }
     }
 
 }
