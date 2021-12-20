537bb4e729c415f144b0f7fa37e165feaf83abac
quibbler01 <a2639168845@163.com>
2021-12-20 13:07:55 +0800

    2021-12-21 引用androidx appcompat库，新增跳转Notification通知系统设置界面，及判断是否开启Notification方法

--- AndroidTools/src/main/java/com/quibbler/androidtools/packages/settings/Settings.kt
+++ AndroidTools/src/main/java/com/quibbler/androidtools/packages/settings/Settings.kt
@@ -1,9 +1,12 @@
 package com.quibbler.androidtools.packages.settings
 
+import android.app.NotificationManager
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
+import android.os.Build
 import android.provider.Settings
+import androidx.core.app.NotificationManagerCompat
 
 public fun openAppDetails(context: Context) {
     openInstalledAppDetails(context, context.packageName)
@@ -17,3 +20,25 @@ public fun openInstalledAppDetails(context: Context, packageName: String) {
     context.startActivity(intent)
 }
 
+public fun openAppNotification(context: Context) {
+    openAppNotification(context, context.packageName)
+}
+
+public fun openAppNotification(context: Context, packageName: String) {
+    val intent: Intent = Intent()
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+        intent.action = Settings.ACTION_APP_NOTIFICATION_SETTINGS
+        intent.putExtra(Settings.EXTRA_APP_PACKAGE, packageName);
+        //intent.putExtra(Settings.EXTRA_CHANNEL_ID, context.applicationInfo.uid);
+    } else {
+        intent.action = Settings.ACTION_APPLICATION_DETAILS_SETTINGS
+        intent.putExtra("app_package", packageName)
+        //intent.putExtra("app_uid", context.applicationInfo.uid)
+    }
+    context.startActivity(intent)
+}
+
+public fun isNotificationsEnabled(context: Context): Boolean {
+    val notificationManager = NotificationManagerCompat.from(context)
+    return notificationManager.areNotificationsEnabled()
+}
