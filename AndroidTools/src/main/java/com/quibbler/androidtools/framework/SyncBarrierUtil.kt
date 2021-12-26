9581a91fdcb4e48a596e9e0c06ca9e667624190f
quibbler01 <a2639168845@163.com>
2021-12-27 00:11:46 +0800

    添加tokens集合保存所有同步屏障Token

--- AndroidTools/src/main/java/com/quibbler/androidtools/framework/SyncBarrierUtil.kt
+++ AndroidTools/src/main/java/com/quibbler/androidtools/framework/SyncBarrierUtil.kt
@@ -3,10 +3,17 @@ package com.quibbler.androidtools.framework
 import android.annotation.SuppressLint
 import android.os.Looper
 import android.os.MessageQueue
+import org.jetbrains.annotations.TestOnly
 import java.lang.Exception
+import java.util.concurrent.CopyOnWriteArraySet
 
 @Deprecated("Never use SyncBarrier in your app.")
 object SyncBarrierUtil {
+
+    /**
+     * global [Set] record all SyncBarrier Tokens.
+     */
+    private val tokens: MutableSet<Int> = CopyOnWriteArraySet<Int>()
 
     @SuppressLint("DiscouragedPrivateApi")
     fun postSyncBarrier(): Int {
@@ -14,6 +21,7 @@ object SyncBarrierUtil {
         try {
             val method = MessageQueue::class.java.getDeclaredMethod("postSyncBarrier")
             token = method.invoke(Looper.getMainLooper().queue) as Int
+            tokens.add(token)
         } catch (ignore: Exception) {
         }
         return token
@@ -21,10 +29,22 @@ object SyncBarrierUtil {
 
     @SuppressLint("DiscouragedPrivateApi")
     fun removeSyncBarrier(token: Int) {
+        if (!tokens.contains(token)) return
         try {
             val method = MessageQueue::class.java.getDeclaredMethod("removeSyncBarrier", Int::class.javaPrimitiveType)
             method.invoke(Looper.getMainLooper().queue, token)
+            tokens.remove(token)
         } catch (ignore: Exception) {
+        }
+    }
+
+    /**
+     * remove all SyncBarrier.
+     */
+    @TestOnly
+    public fun releaseAllSyncBarrier() {
+        for (token in tokens) {
+            removeSyncBarrier(token)
         }
     }
 
