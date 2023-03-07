8003416573f3912ef2ef0b4d23fa80e79f589bc8
quibbler01 <fluent0418@gmail.com>
2023-03-07 22:23:17 +0800

    SystemProperties utils,get and set SystemProperties

--- AndroidTools/src/main/java/com/quibbler/androidtools/framework/SystemPropertiesUtils.kt
+++ AndroidTools/src/main/java/com/quibbler/androidtools/framework/SystemPropertiesUtils.kt
@@ -1,14 +1,44 @@
 package com.quibbler.androidtools.framework
 
-import android.content.Context
+import android.util.Log
 
-private const val SystemPropertiesClassName = "android.os.SystemProperties"
+const val SystemPropertiesClassName = "android.os.SystemProperties"
 
 fun getProperty(name: String, defaultValue: String = ""): String {
     try {
         val clazz = Class.forName(SystemPropertiesClassName)
-        val get = clazz.getDeclaredMethod("get", String.javaClass, String.javaClass)
+        val get = clazz.getMethod("get", String::class.java, String::class.java)
         return get.invoke(clazz, name, defaultValue) as String
+    } catch (ignore: Exception) {
+    }
+    return defaultValue
+}
+
+fun getProperty(name: String, defaultValue: Int = -1): Int {
+    try {
+        val clazz = Class.forName(SystemPropertiesClassName)
+        val get = clazz.getMethod("get", String::class.java, Int::class.java)
+        return get.invoke(clazz, name, defaultValue) as Int
+    } catch (ignore: Exception) {
+    }
+    return defaultValue
+}
+
+fun getProperty(name: String, defaultValue: Long = -1L): Long {
+    try {
+        val clazz = Class.forName(SystemPropertiesClassName)
+        val get = clazz.getMethod("get", String::class.java, Long::class.java)
+        return get.invoke(clazz, name, defaultValue) as Long
+    } catch (ignore: Exception) {
+    }
+    return defaultValue
+}
+
+fun getProperty(name: String, defaultValue: Boolean = false): Boolean {
+    try {
+        val clazz = Class.forName(SystemPropertiesClassName)
+        val get = clazz.getMethod("get", String::class.java, Boolean::class.java)
+        return get.invoke(clazz, name, defaultValue) as Boolean
     } catch (ignore: Exception) {
     }
     return defaultValue
@@ -17,7 +47,7 @@ fun getProperty(name: String, defaultValue: String = ""): String {
 fun setProperty(key: String, value: String) {
     try {
         val clazz = Class.forName(SystemPropertiesClassName)
-        val set = clazz.getDeclaredMethod("set", String.javaClass, String.javaClass)
+        val set = clazz.getMethod("set", String::class.java, String::class.java)
         set.invoke(clazz, key, value)
     } catch (ignore: Exception) {
     }
