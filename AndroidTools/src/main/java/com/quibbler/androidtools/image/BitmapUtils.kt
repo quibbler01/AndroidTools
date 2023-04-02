647cbe2cf2da8d460d09ff801a33e130dbfe920a
quibbler01 <fluent0418@gmail.com>
2023-04-02 11:13:06 +0800

    change object to kotlin file

--- AndroidTools/src/main/java/com/quibbler/androidtools/image/BitmapUtils.kt
+++ AndroidTools/src/main/java/com/quibbler/androidtools/image/BitmapUtils.kt
@@ -5,22 +5,20 @@ import android.graphics.Bitmap
 import android.os.Build
 
 /**
- * This tool class is used to store tool methods related to pictures
+ * in this tool is used to store tool methods related to pictures
  */
-object BitmapUtils {
 
-    /**
-     * Calculate the memory size of the given [Bitmap]
-     */
-    @SuppressLint("ObsoleteSdkInt")
-    fun calculateBitmapSize(bitmap: Bitmap?): Int {
-        return when {
-            bitmap == null -> 0         //return 0 when bitmap is null
-            bitmap.isRecycled -> 0      //or bitmap is recycled
-            Build.VERSION.SDK_INT > Build.VERSION_CODES.KITKAT -> bitmap.allocationByteCount
-            Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR1 -> bitmap.byteCount
-            else -> bitmap.rowBytes * bitmap.height
-        }
+/**
+ * Calculate the memory size of the given [Bitmap]
+ */
+@SuppressLint("ObsoleteSdkInt")
+fun calculateBitmapSize(bitmap: Bitmap?): Int {
+    return when {
+        bitmap == null -> 0         //return 0 when bitmap is null
+        bitmap.isRecycled -> 0      //or bitmap is recycled
+        Build.VERSION.SDK_INT > Build.VERSION_CODES.KITKAT -> bitmap.allocationByteCount
+        Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR1 -> bitmap.byteCount
+        else -> bitmap.rowBytes * bitmap.height
     }
-
 }
+
